a8d618fcaa3522e0c99282ed50755005c3815df6
alex <alexdotnet@163.com>
2013-06-19 19:20:05 +0800

    add
    add new

---
+++ cocos2d-xDemo/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  cocos2d_xDemoAppController.h
+//  cocos2d-xDemo
+//
+//  Created by geek on 13-6-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ cocos2d-xDemo/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  cocos2d_xDemoAppController.h
+//  cocos2d-xDemo
+//
+//  Created by geek on 13-6-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
